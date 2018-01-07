592124cfc540127ea8eea2ea1544b2e35bab203a
Emanuelle Schiavon <emanuelleschiavon@gmail.com>
2018-01-07 21:28:33 -0200

    Retirando metodos http e adicionando validações

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -19,26 +19,10 @@ class DocumentsController < ApplicationController
 		end
 	end
 
-	def destroy
-		document = Document.find(params[:id])
-		document.destroy
-		render json: {status: 'SUCCESS', message: 'Deleted document', data: document}, status: :ok
-	end
-
-	def update
-		document = Document.find(params[:id])
-		if document.update_attributes(document_params)
-			render json:{status: 'SUCCESS', message: 'Updated document', data: document}, status: :ok
-		else
-			render json:{status: 'ERROR', message: 'Document not updated', data: document.erros}, status: :unprocessable_entity
-		end
-	end
-
 	private
 
 	def document_params
 		params.permit(:title, :content)
-		byebug
 	end
 
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,5 @@
 class Document < ApplicationRecord
-	validates :title, presence: true
+	VALID_FILE_FORMAT = /\A([\w]+)(.pdf)\Z/i
+	validates :title, presence: true, uniqueness: { case_sensitive: false }, lenght: { minimum: 3 }, format: { with: VALID_FILE_FORMAT, message: "Invalid filename" }
 	validates :content, presence: true
 end
