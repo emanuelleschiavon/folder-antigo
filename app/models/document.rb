8ecbf4652b27be0ffe59b104db7eea653a83f43b
Emanuelle Schiavon <emanuelleschiavon@gmail.com>
2018-01-07 22:40:06 -0200

    retirada validações

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,6 @@
 class Document < ApplicationRecord
-	VALID_FILE_FORMAT = /\A([\w]+)(.pdf)\Z/i
-	validates :title, presence: true, uniqueness: { case_sensitive: false }, lenght: { minimum: 3 }, format: { with: VALID_FILE_FORMAT, message: "Invalid filename" }
+	#VALID_FILE_FORMAT = /\A([\w]+)(.pdf)\Z/i
+	validates :title, presence: true
+	#, uniqueness: { case_sensitive: false }, format: { with: VALID_FILE_FORMAT, message: "Invalid filename" }
 	validates :content, presence: true
 end
